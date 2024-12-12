cc1609371dd607348f13ee20c955bdf32fd56ad4
Alfa-alfi <alfa.alfi124@smk.belajar.id>
2024-12-12 13:55:55 +0700

    Add files via upload

--- proses-edit.php
+++ proses-edit.php
@@ -1,28 +1,27 @@
 <?php
+
 session_start(); // Mulai sesi
 include("../koneksi.php"); // Pastikan file koneksi.php benar
 
 // Periksa apakah tombol "simpan" pada form edit ditekan
 if (isset($_POST['simpan'])) {
     // Ambil data dari form
-    $lagu_id = $_POST['lagu_id'];
-    $judul_lagu = $_POST['judul_lagu']; 
-    $artis = $_POST['artis'];
-    $durasi = $_POST['durasi'];
+    $id = $_POST['pengguna_id'];
+    $username = $_POST['username']; 
+    $email = $_POST['email'];
     
     // Buat query untuk memperbarui data lagu
-    $sql = "UPDATE lagu SET
-            judul_lagu='$judul_lagu', 
-            artis='$artis', 
-            durasi='$durasi'
-            WHERE lagu_id=$lagu_id";
+    $sql = "UPDATE pengguna SET
+            username='$username', 
+            email='$email'
+            WHERE pengguna_id=$id";
 
             $query = mysqli_query($db, $sql);
             // Simpan pesan notifikasi dalam sesi berdasarkan hasil query
             if ($query) {
-                $_SESSION['notifikasi'] = "Lagu berhasil diperbarui!";
+                $_SESSION['notifikasi'] = "Pengguna berhasil diperbarui!";
             } else {
-                $_SESSION['notifikasi'] = "Lagu gagal diperbarui!";
+                $_SESSION['notifikasi'] = "Pengguna gagal diperbarui!";
             }
             // Alihkan ke halaman index.php
             header('Location: index.php');
